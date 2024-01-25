72d261bb7c00b602e0e257119739a8b72c0fd480
Jefferson Luís <jefferson20042011@protonmail.com>
2024-01-25 18:02:26 -0300

    Add: negativos.c Mod: soma_impares.c

---
+++ Exercicios/C/negativos.c
@@ -0,0 +1,33 @@
+/*
+Problema "negativos"
+Faça um programa que leia um número inteiro positivo N (máximo = 10) e depois N números inteiros
+e armazene-os em um vetor. Em seguida, mostrar na tela todos os números negativos lidos.
+Exemplo:
+Quantos numeros voce vai digitar? 6
+Digite um numero: 8
+Digite um numero: -2
+Digite um numero: 9
+Digite um numero: 10
+Digite um numero: -3
+Digite um numero: -7
+NUMEROS NEGATIVOS:
+-2
+-3
+-7
+*/
+
+#include <stdio.h>
+
+int main(){
+
+    int i, N;
+    int vet [10];
+
+    printf("Quantos numeros voce vai digitar? ");
+    scanf("%d",&N);
+
+    for (i = 0; i < N; i++) {
+        printf("Digite um numero: ");
+        scanf("%d",&vet[i]);
+    }
+}
--- Exercicios/C/soma_impares.c
+++ Exercicios/C/soma_impares.c
@@ -23,11 +23,26 @@ SOMA DOS IMPARES = 5
 
 int main(){
 
-    int x, y, N, i;
+    int x, y, soma, troca;
 
-    printf("Digite dois numeros:");
-    scanf("%d %d", &x, &x);
-    
+    printf("Digite dois numeros:\n");
+    scanf("%d %d", &x, &y);
 
+    if (x > y) {
+        troca = x;
+        x = y;
+        y = troca;
+    }
 
+    soma = 0;
+
+    for (int i = x+1; i < y; i++) {
+        if (i % 2 != 0) {
+            soma = soma + i;
+        }
+    }
+
+    printf("SOMA DOS IMPARES = %d\n", soma);
+
+    return 0;
 }
